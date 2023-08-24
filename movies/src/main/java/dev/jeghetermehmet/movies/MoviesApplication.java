f72bdae218bdbc0c0732a3873dee012c5331bd72
jeghetermehmet <jeghetermehmet@gmail.com>
2023-08-24 13:31:10 +0200

    second

--- movies/src/main/java/dev/jeghetermehmet/movies/MoviesApplication.java
+++ movies/src/main/java/dev/jeghetermehmet/movies/MoviesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoviesApplication {
-
+//hewy
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
