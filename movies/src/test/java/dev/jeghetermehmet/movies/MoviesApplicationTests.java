520159f20a0351f3fd99ad42d2b5aff79bbae83e
jeghetermehmet <jeghetermehmet@gmail.com>
2023-08-24 13:29:04 +0200

    first commit

---
+++ movies/src/test/java/dev/jeghetermehmet/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jeghetermehmet.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
